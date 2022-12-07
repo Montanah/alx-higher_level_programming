5bc53f095430823432253283376ed6b618746444
Montanah <mititi.isaac@gmail.com>
2022-12-07 14:50:33 -0800

    Task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,6 +4,6 @@ def complex_delete(a_dictionary, value):
     for key in a_dictionary:
         if a_dictionary[key] == value:
             key_list.append(key)
-        for key in key_list:
+    for key in key_list:
             del a_dictionary[key]
     return a_dictionary
