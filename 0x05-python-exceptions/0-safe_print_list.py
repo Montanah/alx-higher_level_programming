bc9bb168b5cebbd05f2d671cd98c72db58960add
Montanah <mititi.isaac@gmail.com>
2022-12-21 04:18:42 -0800

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+
+    for index in range(x):
+        try:
+            print("{}".formart(my_list[index]), end='')
+            num += 1
+        except IndexError:
+            pass
+
+    print()
+    return (num)
