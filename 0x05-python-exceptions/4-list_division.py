8e5924b6a7307f758f25d3b870884e7f0673b847
Montanah <mititi.isaac@gmail.com>
2022-12-21 12:46:49 -0800

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             n = my_list_1[index] / my_list_2[index]
         except TypeError:
             print("wrong type")
-            n = None
+            n = 0
         except ZeroDivisionError:
             print("division by 0")
             n = 0
