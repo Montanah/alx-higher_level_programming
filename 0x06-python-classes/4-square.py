9a9763c39d11a3fe412014c2cbaab76786cc19c9
Montanah <mititi.isaac@gmail.com>
2023-01-04 04:32:14 -0800

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A module contaning a square class"""
+
+
+class Square:
+    """Class representing a square"""
+    def __init__(self, size=0):
+        """"Method initialzing a square of size size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Method that returns the size of the square"""
+        return self.__size
+
+    @sizesetter
+    def size(self, value):
+        """"Method that sets the size of the square"""
+        if isinstance(value, int) is not True:
+            raise TypeError("Value must be an integer")
+        elif value < 0:
+            raise ValueError("Value must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Mtethod that returns the area of the square"""
+        return self.__size * self.__size
