f9ff3631237167cef19469a662c2515caf37f338
Montanah <mititi.isaac@gmail.com>
2023-01-04 07:32:04 -0800

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class representing a square"""
     def __init__(self, size=0):
-        """"Method initialzing a square of size size"""
+        """Method initialzing a square of size size"""
 
         if isinstance(size, int) is not True:
             raise TypeError("Size must be an integer")
