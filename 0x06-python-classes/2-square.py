41057893727ac30c69670c3f909e4a2799f74cf7
Montanah <mititi.isaac@gmail.com>
2023-01-04 03:51:20 -0800

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module contaning a square class"""
+
+
+class Square:
+    """Representing a square"""
+    def __init__(self, size=0):
+        """"Method initialzing a square of size size"""
+
+        if isinstance(size, int) is not True:
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.__size = size
