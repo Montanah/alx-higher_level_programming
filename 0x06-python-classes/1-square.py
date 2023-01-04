6240840919665aca6253ac758b76eddbb1cb9df3
Montanah <mititi.isaac@gmail.com>
2023-01-04 03:41:12 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module contaning a square class"""
+
+
+class Square:
+    """Representing a square"""
+    def __init__(self, size=0):
+        """"Method initialzing a square of size size"""
+        self.__size = size
