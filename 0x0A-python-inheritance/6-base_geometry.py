47d5517a7ba724285d708693cf47816ec3aa98f5
Montanah <mititi.isaac@gmail.com>
2023-01-09 14:25:26 -0800

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""""Module contaning an empty class"""
+
+
+class BaseGeometry:
+    """A class contaning a fucntion gets the area"""
+    def area(self):
+        """A function that can raise exception"""
+        raise Exception("area() is not implemented")
