cf355d642f57418563ecdb6699a3dc90cb06408a
Montanah <mititi.isaac@gmail.com>
2023-01-09 13:23:45 -0800

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""""Module that contains a list subclass"""
+
+
+class MyList(list):
+    """"Subclass containing sorted list"""
+    def print_sorted(self):
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list)
+        del sorted_list
