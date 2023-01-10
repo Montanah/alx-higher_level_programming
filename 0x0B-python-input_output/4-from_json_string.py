8bd47b3c52f878c49fc061137709fd00e5d98cbd
Montanah <mititi.isaac@gmail.com>
2023-01-10 14:17:37 -0800

    Task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Ä module that contains a json function"""
+
+import json
+
+
+def from_json_string(my_str):
+    """a function that returns an object (Python data structure) represented by a JSON string"""
+    return json.loads(my_str)
