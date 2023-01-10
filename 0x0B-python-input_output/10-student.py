65f1f27cb7a220859e7a7065a71795bc22361617
Montanah <mititi.isaac@gmail.com>
2023-01-10 15:08:45 -0800

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Ä module containing a class"""
+
+
+class Student:
+    """Ä class with instances"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ä method that retrieves a dictionary
+        representation of a Student instance"""
+        if isinstance(attrs, list) is True:
+            my_dict = dict()
+            for key in attrs:
+                if key in self.__dict__:
+                    my_dict[key] = self.__dict__[key]
+            return my_dict
+        return self.__dict__
