004d326109150ad3ca413c858498b84de4c9ec2c
Montanah <mititi.isaac@gmail.com>
2023-01-10 13:47:56 -0800

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module containing one function"""
+
+
+def read_file(filename=""):
+    """A function that reads file and prints to stdout"""
+    with open(filename, "r" encoding="utf-8") as f:
+        f.read("")
