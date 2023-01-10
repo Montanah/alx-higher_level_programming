52365d05404a3e6da2c257974e3ec3dc18537908
Montanah <mititi.isaac@gmail.com>
2023-01-10 13:50:18 -0800

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """A function that reads file and prints to stdout"""
     with open(filename, "r" encoding="utf-8") as f:
-        f.read("")
+        print(f.read(), end=" ")
