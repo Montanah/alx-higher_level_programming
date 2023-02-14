ae9ad29281f5818f76e15a62c08a72e2e38c33fa
Montanah <mititi.isaac@gmail.com>
2023-02-14 15:42:57 -0800

    Listing data using specific fields

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Lists all records of second-table with a name value
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
