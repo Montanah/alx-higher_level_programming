06c0174355c4ff14d0b4d94abdb8d46887130c5c
Montanah <mititi.isaac.com>
2023-05-24 15:27:44 -0700

    a script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,23 @@
 #!/usr/bin/node
 
-const request = require('request');
-const syncRequest = require('sync-request');
+const request = require('request-promise');
+
 const url = 'https://swapi-api.alx-tools.com/api/films/';
-request(url + process.argv[2] + '/.json', function (error, response, body) {
-  if (error) {
+
+async function getFilmCharacters (filmId) {
+  try {
+    const response = await request(url + filmId + '/.json');
+    const characters = JSON.parse(response).characters;
+
+    for (const characterUrl of characters) {
+      const characterResponse = await request(characterUrl);
+      const character = JSON.parse(characterResponse);
+      console.log(character.name);
+    }
+  } catch (error) {
     console.log(error);
   }
-  const characters = JSON.parse(body).characters;
-  for (const i in characters) {
-    const res = syncRequest('GET', characters[i]);
-    console.log(JSON.parse(res.body).name);
-  }
-});
+}
+
+const filmId = process.argv[2];
+getFilmCharacters(filmId);
