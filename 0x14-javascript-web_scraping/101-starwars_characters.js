191e44e0ac19560e2a885101b9873014b1cefcc8
Montanah <mititi.isaac.com>
2023-05-24 15:37:08 -0700

    a script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,39 @@
 #!/usr/bin/node
 
-const request = require('request-promise');
+const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 
-async function getFilmCharacters (filmId) {
+function getCharacterName(characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        const character = JSON.parse(body);
+        resolve(character.name);
+      }
+    });
+  });
+}
+
+async function getFilmCharacters(filmId) {
   try {
-    const response = await request(url + filmId + '/.json');
-    const characters = JSON.parse(response).characters;
+    const filmUrl = url + filmId + '/.json';
+    const filmResponse = await new Promise((resolve, reject) => {
+      request(filmUrl, function (error, response, body) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(body);
+        }
+      });
+    });
+    const characters = JSON.parse(filmResponse).characters;
 
     for (const characterUrl of characters) {
-      const characterResponse = await request(characterUrl);
-      const character = JSON.parse(characterResponse);
-      console.log(character.name);
+      const characterName = await getCharacterName(characterUrl);
+      console.log(characterName);
     }
   } catch (error) {
     console.log(error);
@@ -21,3 +42,4 @@ async function getFilmCharacters (filmId) {
 
 const filmId = process.argv[2];
 getFilmCharacters(filmId);
+
