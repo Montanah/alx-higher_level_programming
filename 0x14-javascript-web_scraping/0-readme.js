fbe81f0b8b322809f80739ee286a2db40e4ab5ee
Montanah <mititi.isaac.com>
2023-05-24 11:50:34 -0700

    a script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function(err, content){
-  if (err){
+
+fs.readFile(process.argv[2], 'utf8', function (err, content) {
+  if (err) {
     console.log(err);
   } else {
-   process.stdout.write(content);
-  } 
-})
+    process.stdout.write(content);
+  }
+});
