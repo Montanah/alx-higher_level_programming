aa6b46971040ce7feb7a6e34c5c331c2dc3d37c8
Montanah <mititi.isaac.com>
2023-05-24 12:22:31 -0700

    a script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,9 +2,8 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function(err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
-    console.log( err);
+    console.log(err);
   }
 });
-
