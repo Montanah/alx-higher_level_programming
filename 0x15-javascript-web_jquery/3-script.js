dae827c6ec7641d6a57734b8e1072a39eab15aeb
Montanah <mititi.isaac.com>
2023-05-31 15:56:11 -0700

    Adds a class on click

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,8 +1,8 @@
 <head>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
-$(function (){
-	$(#red_header).click(function () {
-		$(this).css("color", "#FF0000");
+$(function () {
+	$("#red_header").click(function () {
+		$(this).addClass(red);
 	});
 });
